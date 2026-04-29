136385628158aa1ae95165c07c3f426741129167
akzz98 <st10263456@myemeris.edu.za>
2026-04-29 21:52:28 +0200

    Add TypeConverters for GoalStatus to Converters class

--- app/src/main/java/com/example/khulisawallet/data/Converters.kt
+++ app/src/main/java/com/example/khulisawallet/data/Converters.kt
@@ -3,9 +3,18 @@ package com.example.khulisawallet.data
 import androidx.room.TypeConverter
 
 class Converters {
+
+    // CategoryType
     @TypeConverter
     fun fromCategoryType(value: CategoryType): String = value.name
 
     @TypeConverter
     fun toCategoryType(value: String): CategoryType = CategoryType.valueOf(value)
+
+    // GoalStatus
+    @TypeConverter
+    fun fromGoalStatus(value: GoalStatus): String = value.name
+
+    @TypeConverter
+    fun toGoalStatus(value: String): GoalStatus = GoalStatus.valueOf(value)
 }
