b0070ac133b54e9ed19875e954aa895a6adfb6b3
akzz98 <st10263456@myemeris.edu.za>
2026-04-29 12:06:34 +0200

    Populate User class to use data class and add Room entity annotations

--- app/src/main/java/com/example/khulisawallet/data/User.kt
+++ app/src/main/java/com/example/khulisawallet/data/User.kt
@@ -1,4 +1,22 @@
 package com.example.khulisawallet.data
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.Index
 
-class User {
-}
+//Ensure email is unique
+@Entity(
+    tableName = "users",
+    indices = [Index(value = ["email"], unique = true)]
+)
+data class User(
+    //Auto generate the PK, ID
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val firstName: String,
+    val lastName: String,
+    val email: String,
+    val passwordHash: String,
+    val profileImagePath: String? = null,
+    val createdAt: Long = System.currentTimeMillis(),
+    val lastLogin: Long? = null,
+    val isActive: Boolean = true
+)
